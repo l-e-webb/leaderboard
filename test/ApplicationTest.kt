04983b0528a3f37d7b5f0e09acafd5d4c33402ee
Louis Webb <louis.webb5@gmail.com>
2020-08-14 10:26:57 -0700

    Initial commit of project template

---
+++ test/ApplicationTest.kt
@@ -0,0 +1,20 @@
+package com.tangledwebgames
+
+import io.ktor.http.HttpMethod
+import io.ktor.http.HttpStatusCode
+import io.ktor.server.testing.handleRequest
+import io.ktor.server.testing.withTestApplication
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class ApplicationTest {
+    @Test
+    fun testRoot() {
+        withTestApplication({ module(testing = true) }) {
+            handleRequest(HttpMethod.Get, "/").apply {
+                assertEquals(HttpStatusCode.OK, response.status())
+                assertEquals("HELLO WORLD!", response.content)
+            }
+        }
+    }
+}
